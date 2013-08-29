9302f94aa04081a241ba6c0c322d2782f002f128
Joël Charles <joel.charles91@gmail.com>
2013-08-29 10:51:50 +0200

    example file

--- examples.py
+++ examples.py
@@ -16,16 +16,16 @@ def handle1(answer):
     return False
 
 def handle2(answer):
-    print('Swagrid : ' + answer)
+    print('The chosen path : ' + answer)
     return True
 
 def main():
     a = ask.Ask()
-    answer = a.ask(Choices('Swag ?', ['Le sucre', 'Le swag sucré'], handle_choices))
+    answer = a.ask(Choices('Kittens ?', ['Hell yeah!', 'NO!', 'Why not'], handle_choices))
     if answer:
-        a.ask(YesNo('Swagy alors ?', handle1))
+        a.ask(YesNo('Is this real life ?', handle1))
     else:
-        a.ask(Path('Swago ?', handle2))
+        a.ask(Path('Where are you ?', handle2))
 
 if __name__ == '__main__':
     main()
