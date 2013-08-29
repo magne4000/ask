53903474583adb3cb89a8967edcd251f695cd98f
Joël Charles <joel.charles91@gmail.com>
2013-08-29 15:11:09 +0200

    Better examples

--- examples.py
+++ examples.py
@@ -3,6 +3,7 @@
 #
 # Distributed under terms of the MIT license.
 import ask
+import os
 from question import Choices, YesNo, Text, Path
 
 def handle_choices(answer):
@@ -10,22 +11,20 @@ def handle_choices(answer):
         return False
     return True
 
-def handle1(answer):
-    if answer.lower() == 'y':
-        return True
-    return False
-
-def handle2(answer):
-    print('The chosen path : ' + answer)
-    return True
-
 def main():
     a = ask.Ask()
+    answer = a.ask(Choices('Kittens ?', ['Hell yeah!', 'NO!', 'Why not']))
+    print('Without handle function, answer contains the user input value : ' + answer)
     answer = a.ask(Choices('Kittens ?', ['Hell yeah!', 'NO!', 'Why not'], handle_choices))
-    if answer:
-        a.ask(YesNo('Is this real life ?', handle1))
+    print('With handle function, answer contains the computed value (False if 1, else True) : ' + str(answer))
+    print('Now a simple YesNo question :')
+    print(a.ask(YesNo('Is this real life ?')))
+    res = a.ask(Path('What is your current full path ? (can autocomplete path with TAB)'))
+    if res == os.getcwd():
+        print('Yep you\'re there !')
     else:
-        a.ask(Path('Where are you ?', handle2))
+        print('Nope you\'re not there !')
+
 
 if __name__ == '__main__':
     main()
