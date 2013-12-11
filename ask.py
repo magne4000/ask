1d5d0f3d53ee8cc9ffefdb5055d291566dff0ad5
Joël Charles <joel.charles91@gmail.com>
2013-12-11 18:00:43 +0100

    _print function added. Use Ask._print(something) instead of print(something) prevents actual printing if a prompt is displayed.

--- ask.py
+++ ask.py
@@ -3,6 +3,7 @@
 #
 # Distributed under terms of the MIT license.
 from question import Question
+import readline, sys, struct, fcntl, termios
 
 """
 A question/answer based prompt python lib
@@ -10,18 +11,38 @@ A question/answer based prompt python lib
 
 class Ask:
 
+    asking = False
+
     def __init__(self, prompt='-> '):
         self.ps = prompt
 
+    @staticmethod
+    def _print(s, clean=False):
+        """
+        Prints something only if a prompt is not displayed
+        """
+        if not Ask.asking:
+            if clean:
+                sys.stdout.write('\x1b[1A') # Move cursor to previous line
+                sys.stdout.write('\r') # Go to the beginning of the line
+            sys.stdout.write(s+'\n')
+            sys.stdout.flush()
+
     def ask(self, q):
+        """
+        Prints the question and input for user answer
+        """
         print(q.question())
         while True:
+            Ask.asking = True
             q.before_raw_input()
             answer = raw_input(self.ps)
             q.after_raw_input()
             if q.check(answer):
                 a = q.answer(answer)
                 if isinstance(a, Question):
+                    Ask.asking = False
                     return self.ask(a)
                 else:
+                    Ask.asking = False
                     return a
